47da0f20bdf01f262d4613f5a64bbeed15094b29
Damien Buhl (alias daminetreg) <damien.buhl@lecbna.org>
2018-02-18 22:10:40 +0100

    :wink: cling compilation working.

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -4,6 +4,36 @@
 #include <cling/UserInterface/UserInterface.h>
 #include <cling/MetaProcessor/MetaProcessor.h>
 
+/*
+
+  '/opt/softwares/cling_2018-02-17_ubuntu16/lib/libclangFrontendTool.a',
+  '/opt/softwares/cling_2018-02-17_ubuntu16/lib/libclingInterpreter.a',
+  '/opt/softwares/cling_2018-02-17_ubuntu16/lib/libclingMetaProcessor.a',
+  '/opt/softwares/cling_2018-02-17_ubuntu16/lib/libclingUserInterface.a',
+  '/opt/softwares/cling_2018-02-17_ubuntu16/lib/libclingUtils.a',
+  '/opt/softwares/cling_2018-02-17_ubuntu16/lib/libclangDriver.a',
+  '/opt/softwares/cling_2018-02-17_ubuntu16/lib/libclangFrontend.a',
+  '/opt/softwares/cling_2018-02-17_ubuntu16/lib/libclangParse.a',
+  '/opt/softwares/cling_2018-02-17_ubuntu16/lib/libclangSema.a',
+  '/opt/softwares/cling_2018-02-17_ubuntu16/lib/libclangAST.a',
+  '/opt/softwares/cling_2018-02-17_ubuntu16/lib/libclangLex.a',
+  '/opt/softwares/cling_2018-02-17_ubuntu16/lib/libclangSerialization.a',
+  '/opt/softwares/cling_2018-02-17_ubuntu16/lib/libclangCodeGen.a',
+  '/opt/softwares/cling_2018-02-17_ubuntu16/lib/libclangBasic.a',
+  '/opt/softwares/cling_2018-02-17_ubuntu16/lib/libLLVMAnalysis.a',
+  '/opt/softwares/cling_2018-02-17_ubuntu16/lib/libLLVMCore.a',
+  '/opt/softwares/cling_2018-02-17_ubuntu16/lib/libLLVMExecutionEngine.a',
+  '/opt/softwares/cling_2018-02-17_ubuntu16/lib/libLLVMipo.a',
+  '/opt/softwares/cling_2018-02-17_ubuntu16/lib/libLLVMMC.a',
+  '/opt/softwares/cling_2018-02-17_ubuntu16/lib/libLLVMObject.a',
+  '/opt/softwares/cling_2018-02-17_ubuntu16/lib/libLLVMOption.a',
+  '/opt/softwares/cling_2018-02-17_ubuntu16/lib/libLLVMOrcJIT.a',
+  '/opt/softwares/cling_2018-02-17_ubuntu16/lib/libLLVMRuntimeDyld.a',
+  '/opt/softwares/cling_2018-02-17_ubuntu16/lib/libLLVMScalarOpts.a',
+  '/opt/softwares/cling_2018-02-17_ubuntu16/lib/libLLVMSupport.a',
+  '/opt/softwares/cling_2018-02-17_ubuntu16/lib/libLLVMTarget.a',
+  '/opt/softwares/cling_2018-02-17_ubuntu16/lib/libLLVMTransformUtils.a'
+*/
 
 namespace demo {
 
@@ -16,8 +46,8 @@ using v8::Value;
 
 void Method(const FunctionCallbackInfo<Value>& args) {
 
-  int argc = 1;
-  const char* argv[1] = {"cling"};
+  int argc = 3;
+  const char* argv[3] = {"cling", "-I", "/opt/softwares/cling_2018-02-17_ubuntu16/include/"};
 
   // cling
   cling::Interpreter Interp(argc, argv);
