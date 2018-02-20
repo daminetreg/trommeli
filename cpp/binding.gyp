ca5e7e4a182a925ac1a4c3562a874eb1c3cbde92
Damien Buhl (alias daminetreg) <damien.buhl@lecbna.org>
2018-02-20 13:51:55 +0100

    :wink: last state.

--- cpp/binding.gyp
+++ cpp/binding.gyp
@@ -3,40 +3,15 @@
     {
       "target_name": "cling_node",
       "sources": [ "main.cpp" ],
-      "cflags" :  ["-std=c++17"],
+      "cflags" :  ["-std=c++17", "-fPIC", "-DPIC"],
       'include_dirs': [
         '/opt/softwares/cling_2018-02-17_ubuntu16/include',
       ],
       'link_settings': {
         'libraries': [
-          '/opt/softwares/cling_2018-02-17_ubuntu16/lib/libcling.so',
-  '/opt/softwares/cling_2018-02-17_ubuntu16/lib/libclangFrontendTool.a',
-  '/opt/softwares/cling_2018-02-17_ubuntu16/lib/libclingInterpreter.a',
-  '/opt/softwares/cling_2018-02-17_ubuntu16/lib/libclingMetaProcessor.a',
-  '/opt/softwares/cling_2018-02-17_ubuntu16/lib/libclingUserInterface.a',
-  '/opt/softwares/cling_2018-02-17_ubuntu16/lib/libclingUtils.a',
-  '/opt/softwares/cling_2018-02-17_ubuntu16/lib/libclangDriver.a',
-  '/opt/softwares/cling_2018-02-17_ubuntu16/lib/libclangFrontend.a',
-  '/opt/softwares/cling_2018-02-17_ubuntu16/lib/libclangParse.a',
-  '/opt/softwares/cling_2018-02-17_ubuntu16/lib/libclangSema.a',
-  '/opt/softwares/cling_2018-02-17_ubuntu16/lib/libclangAST.a',
-  '/opt/softwares/cling_2018-02-17_ubuntu16/lib/libclangLex.a',
-  '/opt/softwares/cling_2018-02-17_ubuntu16/lib/libclangSerialization.a',
-  '/opt/softwares/cling_2018-02-17_ubuntu16/lib/libclangCodeGen.a',
-  '/opt/softwares/cling_2018-02-17_ubuntu16/lib/libclangBasic.a',
-  '/opt/softwares/cling_2018-02-17_ubuntu16/lib/libLLVMAnalysis.a',
-  '/opt/softwares/cling_2018-02-17_ubuntu16/lib/libLLVMCore.a',
-  '/opt/softwares/cling_2018-02-17_ubuntu16/lib/libLLVMExecutionEngine.a',
-  '/opt/softwares/cling_2018-02-17_ubuntu16/lib/libLLVMipo.a',
-  '/opt/softwares/cling_2018-02-17_ubuntu16/lib/libLLVMMC.a',
-  '/opt/softwares/cling_2018-02-17_ubuntu16/lib/libLLVMObject.a',
-  '/opt/softwares/cling_2018-02-17_ubuntu16/lib/libLLVMOption.a',
-  '/opt/softwares/cling_2018-02-17_ubuntu16/lib/libLLVMOrcJIT.a',
-  '/opt/softwares/cling_2018-02-17_ubuntu16/lib/libLLVMRuntimeDyld.a',
-  '/opt/softwares/cling_2018-02-17_ubuntu16/lib/libLLVMScalarOpts.a',
-  '/opt/softwares/cling_2018-02-17_ubuntu16/lib/libLLVMSupport.a',
-  '/opt/softwares/cling_2018-02-17_ubuntu16/lib/libLLVMTarget.a',
-  '/opt/softwares/cling_2018-02-17_ubuntu16/lib/libLLVMTransformUtils.a'
+          "-fPIC",
+          '-L/home/daminetreg/workspace/trommeli/cling/src/build/lib',
+          '-lcling'
         ],
       }
     }
--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,8 +1,12 @@
 // hello.cc
 #include <node.h>
 #include <cling/Interpreter/Interpreter.h>
-#include <cling/UserInterface/UserInterface.h>
-#include <cling/MetaProcessor/MetaProcessor.h>
+//#include <cling/UserInterface/UserInterface.h>
+//#include <cling/MetaProcessor/MetaProcessor.h>
+#include <cling/Interpreter/Value.h>
+#include <cling/Interpreter/Interpreter.h>
+#include <cling/Interpreter/Value.h>
+#include <cling/Utils/Casting.h>
 #include <cmath>
 
 /*
@@ -56,27 +60,48 @@ using v8::Handle;
 using v8::HandleScope;
 using v8::Array;
 
-void Method(const FunctionCallbackInfo<Value>& args) {
+std::vector<double> plotted(10);
 
-  int argc = 3;
-  const char* argv[3] = {"cling", "-I", "/opt/softwares/cling_2018-02-17_ubuntu16/include/"};
+cling::Interpreter* get_interp() {
+  static int argc = 2;
+  static const char* argv[2] = {"cling", "-I/home/daminetreg/workspace/trommeli/cling/src/tools/cling/include/" };
 
   // cling
-  cling::Interpreter Interp(argc, argv);
+  static cling::Interpreter Interp(argc, argv);
+
+  return &Interp;
+}
+
+/*cling::UserInterface* get_cling() {
   //const cling::InvocationOptions& Opts = Interp.getOptions();
-  cling::UserInterface Ui(Interp);
+  static cling::UserInterface Ui(*get_interp());
+  return &Ui;
+}*/
+
+void prepare(const FunctionCallbackInfo<Value>& args) {
+
+  auto& interp = *get_interp();
 
   std::string Line = "#include <iostream>";
   cling::Interpreter::CompilationResult compRes;
 
-  /*int indent =*/ Ui.getMetaProcessor()->process(Line, compRes);
+  /*int indent =*/ interp.process(Line);
 
-  Line = R"(std::cout << "hello world" << std::endl;)";
-  /*indent =*/ Ui.getMetaProcessor()->process(Line, compRes);
+  Line = R"(std::cout << "hello from cling embedded in Electron" << std::endl;)";
+  /*indent =*/ interp.process(Line);
 
-  // Node stuffs
-  Isolate* isolate = args.GetIsolate();
-  args.GetReturnValue().Set(String::NewFromUtf8(isolate, "world"));
+  Line = "#include <vector>";
+  interp.process(Line);
+  interp.declare("std::vector<double> plotted;");
+
+  Line = "#include <algorithm>";
+  interp.process(Line);
+  //Line = "std::vector< double > plotted(10);";
+  //interp.process(Line, compRes);
+
+  //Line = "std::generate_n(plotted.begin(), plotted.end(), [](){ return 0; });";
+  //interp.process(Line, compRes);
+
 }
 
 void ExecuteFunction(const FunctionCallbackInfo<Value>& args) {
@@ -97,19 +122,12 @@ void ExecuteFunction(const FunctionCallbackInfo<Value>& args) {
     return;
   }
 
-  static int argc = 3;
-  static const char* argv[3] = {"cling", "-I", "/opt/softwares/cling_2018-02-17_ubuntu16/include/"};
-
-  // cling
-  static cling::Interpreter Interp(argc, argv);
-  //const cling::InvocationOptions& Opts = Interp.getOptions();
-  static cling::UserInterface Ui(Interp);
+  auto& interp = *get_interp();
 
   std::string Line (*v8::String::Utf8Value(args[0]->ToString()));
   cling::Interpreter::CompilationResult compRes;
-  /*int indent =*/ Ui.getMetaProcessor()->process(Line, compRes);
-
-  /*indent =*/ Ui.getMetaProcessor()->process(Line, compRes);
+  //cling::Value val;
+  interp.process(Line/*, &val)*/);
 
   // Node stuffs
   //args.GetReturnValue().Set(String::NewFromUtf8(isolate, "world"));
@@ -121,16 +139,16 @@ Handle<Array> NewPointArray(Isolate* isolate, double x, double y, double z) {
   HandleScope handle_scope(isolate);
 
   // Create a new empty array.
-  Handle<Array> array = Array::New(isolate, 3);
+  Handle<Array> array = Array::New(isolate, 1);
 
   // Return an empty result if there was an error creating the array.
   if (array.IsEmpty())
     return Handle<Array>();
 
   // Fill out the values
-  array->Set(0, Number::New(isolate, x));
+  /*array->Set(0, Number::New(isolate, x));*/
   array->Set(1, Number::New(isolate, y));
-  array->Set(2, Number::New(isolate, z));
+  /*array->Set(2, Number::New(isolate, z));*/
 
   // Return the value through Close.
   return array;
@@ -154,9 +172,23 @@ void PlotFunction(const FunctionCallbackInfo<Value>& args) {
     return;
   }
 
+  //auto& interp = *get_interp();
+  //cling::Interpreter::CompilationResult compRes;
+  //cling::Value plotted_ptr;
+  //std::string Line = "&plotted;";
+  //interp.process(Line, &plotted_ptr);
+  //auto* plotted = reinterpret_cast<std::vector<double>*>(plotted_ptr.getPtr());
+
+
   // Perform the operation
-  double x = args[0]->NumberValue();
-  auto value  = NewPointArray(isolate, 0.1 * x, 0.1 * x + sin(x), 0.1*x + cos(x));
+  size_t x = 1;//(size_t)args[0]->NumberValue();
+  
+  auto value  = NewPointArray(isolate, 
+    0.1 * x,
+    plotted.at(x),
+    //0.1 * x + sin(x),
+    0.1*x + cos(x)
+  );
   //Local<Number> num = Number::New(isolate, value);
 
   // Set the return value (using the passed in
@@ -165,7 +197,7 @@ void PlotFunction(const FunctionCallbackInfo<Value>& args) {
 }
 
 void init(Local<Object> exports) {
-  NODE_SET_METHOD(exports, "hello", Method);
+  NODE_SET_METHOD(exports, "prepare", prepare);
   NODE_SET_METHOD(exports, "execute", ExecuteFunction);
   NODE_SET_METHOD(exports, "plot_function", PlotFunction);
 }
